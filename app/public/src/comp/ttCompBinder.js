4617746211df90325c81a1825d7a622cd9262a8d
SmallAiTT <536762164@qq.com>
2014-08-06 16:18:40 +0800

    提交comp的初步绑定实现。

---
+++ app/public/src/comp/ttCompBinder.js
@@ -0,0 +1,70 @@
+tt._compPanelsMap = {};
+tt.bindCompPanels = function(widgetClass, panels){
+    tt._compPanelsMap[widgetClass.__className] = panels;
+}
+tt._compPanelsForImg = [{
+    name : "panel1",
+    text : "属性面板1",
+    rows : [
+        { name : "x", text : "x:", clazz : "tt.InputComp", props : {"class" : "easyui-numberbox"} },
+        { name : "y", text : "y:", clazz : "tt.InputComp", props : {"class" : "easyui-numberbox"} },
+        { name : "scaleX", text : "scaleX:", clazz : "tt.InputComp", props : {"class" : "easyui-numberbox"} },
+        { name : "scaleY", text : "scaleY:", clazz : "tt.InputComp", props : {"class" : "easyui-numberbox"} },
+        { name : "rotationX", text : "rotationX", clazz : "tt.InputComp", props : {"class" : "easyui-numberbox"} },
+        { name : "color", text : "color", clazz : "tt.ColorComp", props : {"class" : "tt-color"} }
+    ]
+}]
+
+tt.bindCompPanels(tt.ImgWidget, tt._compPanelsForImg);
+tt.createCompPanel = function(cfg){
+    var temp = '<div><div class="tt-panel-title" name="%s">%s</div></div>';
+    var $container = $('<div class="tt-panel-container"></div>');
+    var $div = $(cc.formatStr(temp, cfg.name, cfg.text));
+    $div.append($container);
+    var rows = cfg.rows;
+    for(var i = 0, li = rows.length; i < li; i++){
+        var itemi = rows[i];
+        var Class = eval('(' + itemi.clazz + ')');
+        var $row = $("<div></div>");
+        $container.append($row);
+        var comp = Class.create(itemi);
+        $row.append(comp.getEle());
+    }
+    return $div;
+};
+
+tt.createCompPanels = function(widget){
+    var cfgs = tt._compPanelsMap[widget.__className];
+    if(!cfgs) return;
+    var $compPanelRoot = $("#compPanelRoot");
+    $compPanelRoot.html("");
+    for(var i = 0, li = cfgs.length; i < li; i++){
+        $compPanelRoot.append(tt.createCompPanel(cfgs[i]));
+    }
+
+
+    $(".tt-color").colpick({
+        layout:'hex',
+        colorScheme:'dark',
+        onShow : function(){
+            $(this).colpickSetColor(this.value);
+        },
+        onHide : function(){
+        },
+        onChange:function(hsb,hex,rgb,el,bySetColor) {
+            var $input = $(el);
+            $input.val("#" + hex);
+            $input[0]._comp._setWidgetProp($input);
+        },
+        onSubmit : function(hsb,hex,rgb,el){
+            var $input = $(el);
+            $input.val("#" + hex);
+            $input[0]._comp._setWidgetProp($input);
+        },
+        onRestoreOriginal : function(hsb,hex,rgb,el){
+            var $input = $(el);
+            $input.val("#" + hex);
+            $input[0]._comp._setWidgetProp($input);
+        }
+    });
+}
